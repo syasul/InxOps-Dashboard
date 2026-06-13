6f0e8020854c21c802f0e5be401df5fa5a163258
syasul <githubku4@gmail.com>
2026-06-13 20:09:25 +0700

    feat: add --ignore-platform-reqs flag to composer install command in DeployProjectJob

--- app/Jobs/DeployProjectJob.php
+++ app/Jobs/DeployProjectJob.php
@@ -119,7 +119,7 @@ class DeployProjectJob implements ShouldQueue
             ];
             
             // Reconstruct the composer command with its arguments
-            $commands[1] = array_merge($commands[1], ['install', '--no-interaction', '--prefer-dist', '--optimize-autoloader']);
+            $commands[1] = array_merge($commands[1], ['install', '--no-interaction', '--prefer-dist', '--optimize-autoloader', '--ignore-platform-reqs']);
 
             // 2. Setup .env if missing
             if (!\Illuminate\Support\Facades\File::exists($path . '/.env')) {
